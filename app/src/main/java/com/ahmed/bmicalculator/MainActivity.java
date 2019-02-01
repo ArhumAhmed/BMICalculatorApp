de519df99cd1e22f4258e6f145f87ed098635f69
Arhum <ft8815@wayne.edu>
2019-01-31 23:37:02 -0500

    Metric is now working. I have views for the inputs and unitViews toggle from visible to gone based on which unit is selected. I also have a flag set based on the unit to tell the calculate method what inputs to take and how to calculate them. At this point, everything is working properly. Just need to add icons.

--- app/src/main/java/com/ahmed/bmicalculator/MainActivity.java
+++ app/src/main/java/com/ahmed/bmicalculator/MainActivity.java
@@ -20,6 +20,18 @@ import com.google.android.gms.ads.MobileAds;
 public class MainActivity extends AppCompatActivity {
 
     TextView bmi;
+    TextView kgView;
+    TextView lbsView;
+    TextView ftView;
+    TextView inView;
+    TextView cmView;
+
+    EditText weight; //lbs
+    EditText kg;
+    EditText feet;
+    EditText inches;
+    EditText cm;
+    boolean flag; //Flag for switching calculations between imperial and metric units
     Spinner spinner;  //Create Spinner object for dropdown
     ArrayAdapter<CharSequence> adapter;
     private AdView mAdView; //Ad object
@@ -27,7 +39,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initialize Views
         bmi=findViewById(R.id.bmiOutput);
+        kgView=findViewById(R.id.kgView);
+        lbsView=findViewById(R.id.lbsView);
+        ftView=findViewById(R.id.ftView);
+        inView=findViewById(R.id.inView);
+        cmView=findViewById(R.id.cmView);
+        //Initialize Inputs
+        weight=findViewById(R.id.weight);
+        kg=findViewById(R.id.kg);
+        feet=findViewById(R.id.feet);
+        inches=findViewById(R.id.inches);
+        cm=findViewById(R.id.cm);
         //Ads
         MobileAds.initialize(this, "ca-app-pub-3011145604783069~6795661224");
         mAdView = findViewById(R.id.adView);
@@ -44,13 +68,39 @@ public class MainActivity extends AppCompatActivity {
                 switch (i)   //Switch case to determine which period is selected
                 {
                     case 0:   //Imperial selected
-
+                        lbsView.setVisibility(View.VISIBLE);  //Shows "lbs"
+                        ftView.setVisibility(View.VISIBLE); //Shows "ft"
+                        inView.setVisibility(View.VISIBLE); //Shows "in"
+                        feet.setVisibility(View.VISIBLE); //Shows feet input
+                        inches.setVisibility(View.VISIBLE); //Shows inches input
+                        weight.setVisibility(View.VISIBLE); //Shows lbs input
+                        cm.setVisibility(View.GONE); //Hides cm input
+                        kg.setVisibility(View.GONE); //Hides kg input
+                        kgView.setVisibility(View.GONE); //Hides "kg" as units
+                        cmView.setVisibility(View.GONE); //Hides "cm" as units
+                        flag = true;
+                        resetColor(); //Resets whatever category/range was highlighted from the previous unit version
+                        bmi.setText("");
                         break;
                     case 1:   //Metric Selected
+                        cm.setVisibility(View.VISIBLE); //Shows cm input
+                        kg.setVisibility(View.VISIBLE); //Shows kg input
+                        kgView.setVisibility(View.VISIBLE); //Show "kg" as units
+                        cmView.setVisibility(View.VISIBLE); //Shows "cm" as units
+                        feet.setVisibility(View.INVISIBLE); //Hides feet input
+                        inches.setVisibility(View.INVISIBLE); //Hides inches input
+                        weight.setVisibility(View.INVISIBLE); //Hides lbs input
+                        lbsView.setVisibility(View.INVISIBLE);  //Hides "lbs"
+                        ftView.setVisibility(View.INVISIBLE); //Hides "ft"
+                        inView.setVisibility(View.INVISIBLE); //hides "in"
+                        resetColor(); //Resets whatever category/range was highlighted from the previous unit version
+                        bmi.setText("");
+                        flag=false;
 
                         break;
 
                     default:   // Should never occur, but if so, treat as imperial
+                        flag=true;
                 }
             }
 
@@ -61,13 +111,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void calculate(View v)
+    public void resetColor()
     {
-        //Define views
-        TextView bmi=findViewById(R.id.bmiOutput);
-        EditText weight = findViewById(R.id.weight);
-        EditText feet=findViewById((R.id.feet));
-        EditText inches=findViewById(R.id.inches);
         //Category Views
         TextView cat1=findViewById(R.id.cat1);
         TextView cat2=findViewById(R.id.cat2);
@@ -89,113 +134,161 @@ public class MainActivity extends AppCompatActivity {
         //View for keeping color
         TextView category = findViewById(R.id.catView);
         ColorStateList oldColors =  category.getTextColors();
-        //Extract inputs as Strings
-        String weightTemp = weight.getText().toString(); //Weight
-        String feetTemp= feet.getText().toString(); //Feet
-        String inchesTemp = inches.getText().toString(); //Inches
 
-        //Verify no blank inputs
-        if (weightTemp.matches("")) //If nothing inputted for weight, display warning toast
-        {
-            Toast.makeText(getApplicationContext(),"Enter a number for weight!",Toast.LENGTH_SHORT).show();
-        }
+        //Reset any changed textviews back to original colors
+        cat1.setTextColor(oldColors);
+        cat2.setTextColor(oldColors);
+        cat3.setTextColor(oldColors);
+        cat4.setTextColor(oldColors);
+        cat5.setTextColor(oldColors);
+        cat6.setTextColor(oldColors);
+        cat7.setTextColor(oldColors);
+        cat8.setTextColor(oldColors);
+        range1.setTextColor(oldColors);
+        range2.setTextColor(oldColors);
+        range3.setTextColor(oldColors);
+        range4.setTextColor(oldColors);
+        range5.setTextColor(oldColors);
+        range6.setTextColor(oldColors);
+        range7.setTextColor(oldColors);
+        range8.setTextColor(oldColors);
+    }
+    public void calculate(View v) {
+        //Define views
+        TextView bmi = findViewById(R.id.bmiOutput);
+        bmi.setText("");
+        //Category Views
+        TextView cat1 = findViewById(R.id.cat1);
+        TextView cat2 = findViewById(R.id.cat2);
+        TextView cat3 = findViewById(R.id.cat3);
+        TextView cat4 = findViewById(R.id.cat4);
+        TextView cat5 = findViewById(R.id.cat5);
+        TextView cat6 = findViewById(R.id.cat6);
+        TextView cat7 = findViewById(R.id.cat7);
+        TextView cat8 = findViewById(R.id.cat8);
+        //Range Views
+        TextView range1 = findViewById(R.id.range1);
+        TextView range2 = findViewById(R.id.range2);
+        TextView range3 = findViewById(R.id.range3);
+        TextView range4 = findViewById(R.id.range4);
+        TextView range5 = findViewById(R.id.range5);
+        TextView range6 = findViewById(R.id.range6);
+        TextView range7 = findViewById(R.id.range7);
+        TextView range8 = findViewById(R.id.range8);
 
-        else if (feetTemp.matches("")) //If nothing inputted for feet, display warning toast
-        {
-            Toast.makeText(getApplicationContext(),"Enter a number for feet!",Toast.LENGTH_SHORT).show();
-        }
+        // bmiCalc answer
+        double bmiCalc = 0.0;
 
-        else  //All fields are valid
+        boolean inputValidate;
+        if (flag) //If imperial
         {
-            //If inches field empty, set to default 0
-            if (inchesTemp.matches("")) //If nothing inputted for inches, set to 0
+            EditText weight = findViewById(R.id.weight);
+            EditText feet = findViewById((R.id.feet));
+            EditText inches = findViewById(R.id.inches);
+            //Extract inputs as Strings
+            String weightTemp = weight.getText().toString(); //Weight
+            String feetTemp = feet.getText().toString(); //Feet
+            String inchesTemp = inches.getText().toString(); //Inches
+
+            //Verify no blank inputs
+            if (weightTemp.matches("")) //If nothing inputted for weight, display warning toast
             {
-                inchesTemp="0";
+                Toast.makeText(getApplicationContext(), "Enter a number for weight!", Toast.LENGTH_SHORT).show();
+                inputValidate = false;
+            } else if (feetTemp.matches("")) //If nothing inputted for feet, display warning toast
+            {
+                Toast.makeText(getApplicationContext(), "Enter a number for feet!", Toast.LENGTH_SHORT).show();
+                inputValidate = false;
+            } else  //All fields are valid
+            {
+                inputValidate = true;
+                //If inches field empty, set to default 0
+                if (inchesTemp.matches("")) //If nothing inputted for inches, set to 0
+                {
+                    inchesTemp = "0";
+                }
+
+                //Parse inputs as their proper data type
+                double weightNum = Double.parseDouble(weightTemp); //Weight
+                int inchesNum = Integer.parseInt(feetTemp); //Feet
+                inchesNum *= 12; //Feet to inches
+                inchesNum += Integer.parseInt(inchesTemp); //Add inches to inchesNum
+
+                //Determining BMI
+                bmiCalc = 703.0 * weightNum;
+                bmiCalc /= (inchesNum * inchesNum);
+                bmi.setText(String.format("%.1f", bmiCalc));
             }
+        } else  //If metric
+        {
+            EditText weightKG = findViewById(R.id.kg); //Weight in kg
+            EditText cm = findViewById((R.id.cm));
+            //Extract inputs as Strings
+            String weightTemp = weightKG.getText().toString(); //Weight (kg)
+            String cmTemp = cm.getText().toString(); //Cm
 
-            //Parse inputs as their proper data type
-            double weightNum= Double.parseDouble(weightTemp); //Weight
-            int inchesNum = Integer.parseInt(feetTemp); //Feet
-            inchesNum *=12; //Feet to inches
-            inchesNum += Integer.parseInt(inchesTemp); //Add inches to inchesNum
-
-            //Determining BMI
-            double bmiCalc = 703.0 * weightNum;
-            bmiCalc /= (inchesNum*inchesNum);
-            bmi.setText(String.format("%.1f", bmiCalc));
-
-
-            //Reset any changed textviews back to original colors
-            cat1.setTextColor(oldColors);
-            cat2.setTextColor(oldColors);
-            cat3.setTextColor(oldColors);
-            cat4.setTextColor(oldColors);
-            cat5.setTextColor(oldColors);
-            cat6.setTextColor(oldColors);
-            cat7.setTextColor(oldColors);
-            cat8.setTextColor(oldColors);
-            range1.setTextColor(oldColors);
-            range2.setTextColor(oldColors);
-            range3.setTextColor(oldColors);
-            range4.setTextColor(oldColors);
-            range5.setTextColor(oldColors);
-            range6.setTextColor(oldColors);
-            range7.setTextColor(oldColors);
-            range8.setTextColor(oldColors);
-
-
-
-            //Change color of selected category and BMI range to pink
-            if (bmiCalc<16.0)
+            //Verify no blank inputs
+            if (weightTemp.matches("")) //If nothing inputted for weight, display warning toast
             {
+                Toast.makeText(getApplicationContext(), "Enter a number for weight!", Toast.LENGTH_SHORT).show();
+                inputValidate = false;
+            } else if (cmTemp.matches("")) //If nothing inputted for cm, display warning toast
+            {
+                Toast.makeText(getApplicationContext(), "Enter a number for cm!", Toast.LENGTH_SHORT).show();
+                inputValidate = false;
+            } else  //All fields are valid
+            {
+                inputValidate = true;
+                //Parse inputs as their proper data type
+                double weightNum = Double.parseDouble(weightTemp); //Weight
+                double cmNum = Double.parseDouble(cmTemp); //CM
+
+                //Determining BMI
+                bmiCalc = (weightNum) / ((cmNum / 100) * (cmNum / 100));
+                if (bmiCalc>5000.0)
+                {
+                    bmi.setText(">5000");
+                }
+                else
+                {
+                    bmi.setText(String.format("%.1f", bmiCalc));
+                }
+            }
+        }
+
+        resetColor();
+
+        //Change color of selected category and BMI range to pink
+        if (inputValidate) {
+            if (bmiCalc < 16.0) {
                 cat1.setTextColor(Color.parseColor("#FFD81B60"));
                 range1.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >= 16.0 && bmiCalc <17.0)
-            {
+            } else if (bmiCalc >= 16.0 && bmiCalc < 17.0) {
                 cat2.setTextColor(Color.parseColor("#FFD81B60"));
                 range2.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >= 17.0 && bmiCalc < 18.5)
-            {
+            } else if (bmiCalc >= 17.0 && bmiCalc < 18.5) {
                 cat3.setTextColor(Color.parseColor("#FFD81B60"));
                 range3.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >= 18.5 && bmiCalc <25.0)
-            {
+            } else if (bmiCalc >= 18.5 && bmiCalc < 25.0) {
                 cat4.setTextColor(Color.parseColor("#FFD81B60"));
                 range4.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >=25.0 && bmiCalc < 30.0)
-            {
+            } else if (bmiCalc >= 25.0 && bmiCalc < 30.0) {
                 cat5.setTextColor(Color.parseColor("#FFD81B60"));
                 range5.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >=30.0 && bmiCalc < 35.0)
-            {
+            } else if (bmiCalc >= 30.0 && bmiCalc < 35.0) {
                 cat6.setTextColor(Color.parseColor("#FFD81B60"));
                 range6.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else if (bmiCalc >=35.0 && bmiCalc <40.0)
-            {
+            } else if (bmiCalc >= 35.0 && bmiCalc < 40.0) {
                 cat7.setTextColor(Color.parseColor("#FFD81B60"));
                 range7.setTextColor(Color.parseColor("#FFD81B60"));
-            }
-
-            else
-            {
+            } else {
                 cat8.setTextColor(Color.parseColor("#FFD81B60"));
                 range8.setTextColor(Color.parseColor("#FFD81B60"));
             }
         }
 
+    }
+
 
 
     }
-}
